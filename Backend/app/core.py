d5ae61251238bf031c1cf38b96795f66d2cc0728
Islene888 <islenezhao@gmail.com>
2025-06-08 16:01:57 -0700

    Remove node_modules from repository

--- Backend/app/core.py
+++ Backend/app/core.py
@@ -3,9 +3,6 @@ import sys, os
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 if root_path not in sys.path:
     sys.path.append(root_path)
-from Bert.Utils.sentiment_utils import analyze_sentiment, calc_emotion_slope, calc_cumulative_gain
-from Bert.Utils.bertscore_utils import evaluate_bertscore
-from Bert.metrics.metrics import session_metrics, to_dataframe, aggregate_summary
 
 from Bert.main_test import run_bert_judge
 import sqlalchemy
